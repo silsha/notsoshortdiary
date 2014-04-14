127791983ab5c477ae97c88851150c7524b847d6
Lukas Martini <hello@lutoma.org>
2014-04-14 12:45:05 +0200

    Indentation and style fixes.

--- list.py
+++ list.py
@@ -21,20 +21,23 @@ def index():
 
 @app.template_filter('strftime')
 def _jinja2_filter_datetime(date, fmt=None):
-    date = dateutil.parser.parse(date)
-    native = date.replace(tzinfo=None)
-    format='%b %d, %Y'
-    return native.strftime(format) 
+	date = dateutil.parser.parse(date)
+	native = date.replace(tzinfo=None)
+	format='%b %d, %Y'
+	return native.strftime(format) 
     
 @app.template_filter()
 @evalcontextfilter
 def linebreaks(eval_ctx, value):
-    """Converts newlines into <p> and <br />s."""
-    value = re.sub(r'\r\n|\r|\n', '\n', value) # normalize newlines
-    paras = re.split('\n{2,}', value)
-    paras = [u'<p>%s</p>' % p.replace('\n', '<br />') for p in paras]
-    paras = u'\n\n'.join(paras)
-    return Markup(paras)
+	"""
+	Converts newlines into <p> and <br />s.
+	"""
+
+	value = re.sub(r'\r\n|\r|\n', '\n', value) # normalize newlines
+	paras = re.split('\n{2,}', value)
+	paras = [u'<p>%s</p>' % p.replace('\n', '<br />') for p in paras]
+	paras = u'\n\n'.join(paras)
+	return Markup(paras)
 
 if __name__ == '__main__':
 	app.run(debug=True)
